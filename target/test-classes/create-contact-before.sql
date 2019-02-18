8657c3708f2696bf94325f5d513e8e5140700698
timyr <timyrle45@gmail.com>
2019-02-18 21:35:40 +0200

    has been deleted some files

--- target/test-classes/create-contact-before.sql
+++
@@ -1,4 +0,0 @@
-delete from usr;
-
-insert into usr(id, username, password, active) values (1, 'Boris', '1999', true);
-insert into usr(id, username, password, active) values (2, 'Victor', '2000', true );
